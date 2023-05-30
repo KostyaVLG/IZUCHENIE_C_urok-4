3bb97f574921fedc1e1f0552fffe3ff46dc48a4c
KostyaVLG <dka1977@yandex.ru>
2023-05-30 19:42:30 +0300

    Reshil_Zadachu_10-05-23

---
+++ ZADACHA-1_U4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число А: ");
+int number1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Введите число В: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int count = number1;
+
+for (int i=1; i < number2; i++)
+{
+count=count*number1;
+}
+
+Console.WriteLine("A в степени B равно: " + count);
---
+++ ZADACHA-3_U4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int size = 8;
+int[] array = new int[size]; // Массив из 8 элементов: в массиве изначально 8 нулей
+
+for (int i = 0; i < size; i++)
+{
+    // array[i] = new Random().Next(0, 2)
+    array[i] = new Random().Next(99);
+    // Console.Write(array[i] + " ");
+}
+
+Console.WriteLine($"{String.Join(", ",array)} -> [ {String.Join(", ",array)} ]");
